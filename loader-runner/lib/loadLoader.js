4483eba0e51f4ebd1149e131547b7e37220fb6c0
sunfutao <sunfutao@didiglobal.com>
2023-07-26 18:45:33 +0800

    feat: add loader-runner

---
+++ loader-runner/lib/loadLoader.js
@@ -0,0 +1,68 @@
+var LoaderLoadingError = require("./LoaderLoadingError");
+var url;
+/**
+ * 加载loader
+ * @param {*} loader
+ * @param {*} callback
+ * @returns {*}
+ */
+module.exports = function loadLoader(loader, callback) {
+	if(loader.type === "module") {
+		try {
+			if(url === undefined) url = require("url");
+			var loaderUrl = url.pathToFileURL(loader.path);
+			// import()引入并执行loader
+			var modulePromise = eval("import(" + JSON.stringify(loaderUrl.toString()) + ")");
+			modulePromise.then(function(module) {
+				handleResult(loader, module, callback);
+			}, callback);
+			return;
+		} catch(e) {
+			callback(e);
+		}
+	} else {
+		try {
+			// require()引入并执行Loader
+			var module = require(loader.path);
+		} catch(e) {
+			// it is possible for node to choke on a require if the FD descriptor
+			// limit has been reached. give it a chance to recover.
+			if(e instanceof Error && e.code === "EMFILE") {
+				var retry = loadLoader.bind(null, loader, callback);
+				if(typeof setImmediate === "function") {
+					// node >= 0.9.0
+					return setImmediate(retry);
+				} else {
+					// node < 0.9.0
+					return process.nextTick(retry);
+				}
+			}
+			return callback(e);
+		}
+		return handleResult(loader, module, callback);
+	}
+};
+
+/**
+ * 格式化loader对象，调用callback
+ * @param {*} loader
+ * @param {*} module
+ * @param {*} callback
+ * @returns {*}
+ */
+function handleResult(loader, module, callback) {
+	if(typeof module !== "function" && typeof module !== "object") {
+		return callback(new LoaderLoadingError(
+			"Module '" + loader.path + "' is not a loader (export function or es6 module)"
+		));
+	}
+	loader.normal = typeof module === "function" ? module : module.default;
+	loader.pitch = module.pitch;
+	loader.raw = module.raw;
+	if(typeof loader.normal !== "function" && typeof loader.pitch !== "function") {
+		return callback(new LoaderLoadingError(
+			"Module '" + loader.path + "' is not a loader (must have normal or pitch function)"
+		));
+	}
+	callback();
+}
